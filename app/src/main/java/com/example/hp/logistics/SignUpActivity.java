b287009147dd9f9265d5013860d93d1f4506a0d6
avnitalati <avnitalati@gmail.com>
2017-02-09 18:44:05 +0530

    Logi commited

---
+++ app/src/main/java/com/example/hp/logistics/SignUpActivity.java
@@ -0,0 +1,17 @@
+package com.example.hp.logistics;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Hp on 2/9/2017.
+ */
+
+public class SignUpActivity extends AppCompatActivity  {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signup);
+    }
+}
